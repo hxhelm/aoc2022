93600e247cd2c1c8bb308e98240f937fd53f7e6b
Wilhelm Widergold <widergold.wilhelm@outlook.de>
2022-12-07 19:56:26 +0100

    add (bad) solutions for day 7

---
+++ src/bin/07.rs
@@ -0,0 +1,226 @@
+use std::collections::{HashMap};
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::{digit1};
+use nom::IResult;
+use nom::multi::separated_list1;
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    size: i32,
+}
+
+#[derive(Debug)]
+struct FileSystem {
+    cwd: String,
+    files: HashMap<String, Vec<File>>,
+}
+
+impl FileSystem {
+    fn cd(&mut self, dir: &str) {
+        if dir == ".." {
+            if self.cwd == "/" {
+                return;
+            }
+
+            let mut vec = self.cwd
+                .split("/")
+                .collect::<Vec<_>>();
+
+            // remove last directory, keep / at end
+            vec.remove(vec.len()-2);
+
+            self.cwd = vec.join("/");
+
+            return;
+        }
+
+        if dir == "/" {
+            self.cwd = dir.to_string();
+            return;
+        }
+
+        self.cwd = [&self.cwd, dir, "/"].join("").to_string();
+    }
+
+    fn mkdir(&mut self, name: &str) {
+        let directory_path = [&self.cwd, name, "/"].join("");
+
+        if self.files.contains_key(&directory_path) {
+            return;
+        }
+
+        self.files.insert(
+            directory_path,
+            vec![],
+        );
+    }
+
+    fn touch(&mut self, filename: &str, filesize: i32) {
+        let directory = self.files.get_mut(self.cwd.as_str()).unwrap();
+        directory.push(File {
+            name: filename.to_string(),
+            size: filesize
+        });
+    }
+
+    fn get_dir_size(&self, dir: &str) -> i32 {
+        let mut size = 0;
+
+        for (directory, files) in self.files.iter() {
+            if !directory.contains(&dir) {
+                continue;
+            }
+
+            size += files.iter()
+                .map(|file| file.size)
+                .sum::<i32>();
+        }
+
+        size
+    }
+}
+
+#[derive(Debug)]
+enum Instruction {
+    ChangeDirectory(String),
+    AddDirectory(String),
+    AddFile(i32, String),
+    Skip,
+}
+
+fn parse_line(input: &str) -> IResult<&str, Instruction> {
+    let (input, line_match) = alt((
+        tag("$ "),
+        tag("dir "),
+        digit1
+    ))(input)?;
+
+    let result = match line_match {
+        "$ " => match input {
+            "ls" => Instruction::Skip,
+            _ => {
+                let dir = input.split(" ").last().unwrap();
+                Instruction::ChangeDirectory(dir.to_string())
+            },
+        },
+        "dir " => Instruction::AddDirectory(input.to_string()),
+        _ => Instruction::AddFile(
+            line_match.parse::<i32>().unwrap(),
+            input.trim().to_string(),
+        ),
+    };
+    Ok((input, result))
+}
+
+fn parse_instruction_lines(input: &str) -> IResult<&str, Vec<Instruction>> {
+    let instructions = input.lines()
+        .map(|line| -> Instruction {
+            let (_, instruction) = parse_line(&line).unwrap();
+
+            instruction
+        })
+        .collect();
+
+    Ok((input, instructions))
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    let starting_working_directory = String::from("/");
+    let mut files = HashMap::new();
+    files.insert(starting_working_directory.clone(), vec![]);
+
+    let mut filesystem = FileSystem {
+        cwd: starting_working_directory,
+        files,
+    };
+
+    let (_, lines) = parse_instruction_lines(input).unwrap();
+
+    for line in lines {
+        match line {
+            Instruction::ChangeDirectory(cmd) => filesystem.cd(&cmd),
+            Instruction::AddDirectory(dir) => filesystem.mkdir(&dir),
+            Instruction::AddFile(size, name) => filesystem.touch(&name, size),
+            Instruction::Skip => continue,
+        };
+    }
+
+    let mut total_size = 0;
+    for (directory, _) in &filesystem.files {
+        let dirsize = &filesystem.get_dir_size(&directory);
+
+        if *dirsize < 100000 {
+            total_size += *dirsize;
+        }
+    }
+
+    Some(total_size)
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    let starting_working_directory = String::from("/");
+    let mut files = HashMap::new();
+    files.insert(starting_working_directory.clone(), vec![]);
+
+    let mut filesystem = FileSystem {
+        cwd: starting_working_directory,
+        files,
+    };
+
+    let (_, lines) = parse_instruction_lines(input).unwrap();
+
+    for line in lines {
+        match line {
+            Instruction::ChangeDirectory(cmd) => filesystem.cd(&cmd),
+            Instruction::AddDirectory(dir) => filesystem.mkdir(&dir),
+            Instruction::AddFile(size, name) => filesystem.touch(&name, size),
+            Instruction::Skip => continue,
+        };
+    }
+
+    let total_space = 70000000;
+    let needed_for_update = 30000000;
+
+    let used = filesystem.get_dir_size("/");
+    let available = total_space - used;
+    let need_to_delete = needed_for_update - available;
+
+    let mut current_dirsize = used;
+
+    for (directory, _) in &filesystem.files {
+        let &dirsize = &filesystem.get_dir_size(&directory);
+
+        if dirsize < need_to_delete || dirsize > current_dirsize {
+            continue
+        }
+
+        current_dirsize = dirsize;
+    }
+
+    Some(current_dirsize)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 7);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 7);
+        assert_eq!(part_one(&input), Some(95437));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 7);
+        assert_eq!(part_two(&input), Some(24933642));
+    }
+}
